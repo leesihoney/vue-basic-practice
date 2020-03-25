6a594593b60d23f30c756a423ff5a717e7add25f
= <siheonl@andrew.cmu.edu>
2020-03-24 20:37:29 -0400

    computed property done

--- main.js
+++ main.js
@@ -2,19 +2,22 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		product: "Socks",
-		image: "./assets/vmSocks-blue.jpg",
-		inStock: true,
+		brand: "Vue Mastery",
+		onSale: true,
+		selectedVariant: 0,
 		details: ["80% cotton", "20% polyester", "Gender-neutral"],
 		variants: [
 			{
 				variantId: 2234,
 				variantColor: "green",
-				variantImage: "./assets/vmSocks-green.jpg"
+				variantImage: "./assets/vmSocks-green.jpg",
+				variantQuantity: 10
 			},
 			{
 				variantId: 2235,
 				variantColor: "blue",
-				variantImage: "./assets/vmSocks-blue.jpg"
+				variantImage: "./assets/vmSocks-blue.jpg",
+				variantQuantity: 0
 			}
 		],
 		cart: 0
@@ -23,8 +26,23 @@ var app = new Vue({
 		addToCart() {
 			this.cart += 1
 		},
-		updateProduct(variantImage) {
-			this.image = variantImage
+		updateProduct(index) {
+			this.selectedVariant = index;
+		}
+	},
+	computed: {
+		title() {
+			return this.brand + " " + this.product;
+		},
+		image() {
+			return this.variants[this.selectedVariant].variantImage;
+		},
+		inStock() {
+			return this.variants[this.selectedVariant].variantQuantity;
+		},
+		sale() {
+			const addString = this.onSale ? " are on sale!" : " are not on sale"
+			return this.brand + ' ' + this.product + addString;
 		}
 	}
 })
